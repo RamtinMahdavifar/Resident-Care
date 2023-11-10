3a1bcbf25fbb491eebeee537694ab9d902683985
Bathiya Ranasinghe <90421548+BathiyaRanasinghe@users.noreply.github.com>
2023-11-09 20:03:39 -0700

    Alternative solution runs faster

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# Azure Cognitive Services API Key
+azure_api_key = 'b47149eda5f34024a9df63d66f97597c'
+
+# Azure Cognitive Services Region (e.g., 'westus', 'eastus', etc.)
+azure_region = 'eastus'
+
+# OpenAI API Key
+openai_api_key = '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'
