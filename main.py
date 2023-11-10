832771b06c3997a58c04c1102fb6ecc09bacc9dd
Bathiya Ranasinghe <90421548+BathiyaRanasinghe@users.noreply.github.com>
2023-11-09 21:02:11 -0700

    Clean code with docstrings

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
-import  streamlit as st
-from streamlit_chat import message
 import os
 import tempfile
 import azure.cognitiveservices.speech as speechsdk
 import openai
 import config
 import pygame
-import textwrap
-from IPython.display import display, Audio
-import streamlit as st
-from IPython.display import Video
-from IPython.display import HTML
-import base64
+from IPython.display import Audio
 import numpy as np
 
-# Initialize the mixer module
+# Initialize the mixer module for audio playback
 pygame.mixer.init()
 
-# Function to play a sound at a given frequency and duration
-
 def beep(frequency, duration):
+    """
+    Play a beep sound at a specified frequency and duration.
+
+    Parameters:
+    frequency (int): The frequency of the beep in Hertz.
+    duration (int): The duration of the beep in milliseconds.
+    """
     # Generate a sound buffer with the given frequency
     sample_rate = 44100
     n_samples = int(round(duration * sample_rate / 1000.0))
@@ -36,57 +34,19 @@ def beep(frequency, duration):
     pygame.time.delay(duration)
     sound.stop()
 
-# Provide the path to your logo image file
-logo = "https://media.discordapp.net/attachments/995431274267279440/1108765311802556456/YellowArrow_PDFs_to_Custom_Chat_Bot_app_illustration_for_a_cove_e8cb1bda-8b00-48df-b8a3-7f41b7ea4d03.png"
-
-
-# Provide the path to your video file
-video_file_path = "Intro Video.mp4"
-
-# Read the video file as bytes
-video_bytes = open(video_file_path, "rb").read()
-
-# Encode the video bytes as base64
-video_base64 = base64.b64encode(video_bytes).decode("utf-8")
-
-# Generate the HTML video tag with auto-play
-video_tag = f"""<video width="250" height="250" controls autoplay>
-    <source src="data:video/mp4;base64,{video_base64}" type="video/mp4">
-    Your browser does not support the video tag.
-</video>
-"""
-
-# Display the video using Streamlit
-st.write(video_tag, unsafe_allow_html=True)
-
-# Apply CSS styling to center the video
-st.markdown(
-    """
-    <style>
-    video {
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-st.sidebar.markdown(
-    """
-    <div style="display: flex; justify-content: center;">
-        <img src="{}" style="width:250px;height:250px;">
-    </div>
-    <hr style='border: none; border-top: 1px solid #ccc; margin: 20px 0px;'>
-    """.format(logo),
-    unsafe_allow_html=True,
-)
-
-# Define the robot emoji
-robot_emoji = "�"
+# Define the robot emoji for display purposes
+robot_emoji = "🤖"
 
 def transcribe_audio(speech_config):
+    """
+    Transcribe audio input from the default microphone using Azure Speech Service.
+
+    Parameters:
+    speech_config (SpeechConfig): The configuration for speech service.
+
+    Returns:
+    str: The transcribed text.
+    """
     audio_config = speechsdk.AudioConfig(use_default_microphone=True)
     speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
 
@@ -94,8 +54,18 @@ def transcribe_audio(speech_config):
     return result.text.strip()
 
 def generate_response(input_text, conversation_history):
+    """
+    Generate a response to the input text using OpenAI's GPT model.
+
+    Parameters:
+    input_text (str): The user's input text to respond to.
+    conversation_history (list): The history of the conversation.
+
+    Returns:
+    str: The generated response text.
+    """
     messages = [
-        {"role": "system", "content": "You are a helpful assistant. Please respond to all input in 50 words or less."},
+        {"role": "system", "content": "You are a helpful assistant."},
     ]
 
     messages.extend(conversation_history)
@@ -113,6 +83,14 @@ def generate_response(input_text, conversation_history):
     return response['choices'][0]['message']['content']
 
 def synthesize_and_save_speech(speech_config, response_text, file_path):
+    """
+    Synthesize speech from text and save it to a file.
+
+    Parameters:
+    speech_config (SpeechConfig): The configuration for speech synthesis.
+    response_text (str): The text to synthesize.
+    file_path (str): The file path to save the synthesized audio.
+    """
     speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
     result = speech_synthesizer.speak_text_async(response_text).get()
 
@@ -120,13 +98,33 @@ def synthesize_and_save_speech(speech_config, response_text, file_path):
         f.write(result.audio_data)
 
 def play_audio(audio_file_path):
-    display(Audio(audio_file_path))
+    """
+    Play an audio file.
+
+    Parameters:
+    audio_file_path (str): The path to the audio file to play.
+    """
+    Audio(audio_file_path)
 
 def remove_temp_files(file_path):
+    """
+    Remove temporary files created during the process.
+
+    Parameters:
+    file_path (str): The path to the file to remove.
+    """
     os.remove(file_path)
 
 def main(stop_keyword="stop", exit_keyword="exit"):
-    st.title("🤖 ChatGPT Voice Assistant Powered by Azure Speech Studio")
+    """
+    Main function to run the Azure Speech Integration with ChatGpt Demo.
+
+    Parameters:
+    stop_keyword (str): The keyword to stop and restart the conversation.
+    exit_keyword (str): The keyword to exit the conversation.
+    """
+    print("Azure Speech Integration with ChatGpt Demo")
+
 
     # Define speech config
     azure_api_key = config.azure_api_key
@@ -138,60 +136,38 @@ def main(stop_keyword="stop", exit_keyword="exit"):
 
     conversation_history = []
 
-    # Increase font size
-    st.markdown("<style>body {font-size: 18px;}</style>", unsafe_allow_html=True)
+    running = True
+    while running:
+        print(robot_emoji + " Listening...")
+        beep(800, 200)  # Play a beep at 800 Hz for 200 milliseconds
 
-    # Sidebar button to start the program
-    st.sidebar.write("Press the 'Start Button' and ask me a question and I will respond...")  # Instruction section
-    if st.sidebar.button("Start Program"):
-       # st.sidebar.write("Please ask me a question and I will respond...")  # Instruction section
-        st.sidebar.write("Note:  You can start your question over by saying 'Stop' during question input...")  # Instruction section
-        st.sidebar.write("You can end the chat session by saying 'Exit'")  # Instruction section
-        while True:
-            st.text(robot_emoji + " Listening...")
-            beep(800, 200)  # Play a beep at 800 Hz for 200 milliseconds
+        input_text = transcribe_audio(speech_config)
+        print(f"You: {input_text}")
 
-            input_text = transcribe_audio(speech_config)
-            wrapped_input = textwrap.fill(input_text, width=90)
-            indented_input = "\n".join(["<div style='text-align: left;'>" + line + "</div>" for line in wrapped_input.splitlines()])
+        if stop_keyword.lower() in input_text.lower():
+            print("Restarting prompt...")
+            conversation_history = []
+            continue
 
-            st.markdown(f"<div style='padding: 30px;'>"
-                        f"<div style='background-color: blue; padding: 10px; border-radius: 5px; color: white; text-align: left;'>"
-                        f"{indented_input}</div>"
-                        f"</div>",
-                        unsafe_allow_html=True)
+        if exit_keyword.lower() in input_text.lower():
+            print("Goodbye for now...")
+            break
 
-            if stop_keyword.lower() in input_text.lower():
-                st.text("Restarting prompt...")
-                conversation_history = []
-                continue
+        response_text = generate_response(input_text, conversation_history)
+        print(robot_emoji + f" Assistant: {response_text}")
 
-            if exit_keyword.lower() in input_text.lower():
-                st.markdown(f"<div style='background-color: white; padding: 10px; border-radius: 5px;'>"
-                            f"<span style='color: black;'>Goodbye for now...</span></div>",
-                            unsafe_allow_html=True)
-                break
+        with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as f:
+            audio_file_path = f.name
 
-            response_text = generate_response(input_text, conversation_history)
-            wrapped_response = textwrap.fill(response_text, width=70)
-            indented_response = "\n".join(["<div style='text-align: left;'>" + line + "</div>" for line in wrapped_response.splitlines()])
+        try:
+            synthesize_and_save_speech(speech_config, response_text, audio_file_path)
+            play_audio(audio_file_path)
+            remove_temp_files(audio_file_path)
+        except Exception as e:
+            print(f"Error: Failed to generate or play audio - {e}")
 
-            st.markdown(f"<div style='background-color: #ADD8E6; padding: 10px; border-radius: 5px; text-align: left; color: black;'>"
-                        f"{indented_response}</div>",
-                        unsafe_allow_html=True)
-
-            with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as f:
-                audio_file_path = f.name
-
-            try:
-                synthesize_and_save_speech(speech_config, response_text, audio_file_path)
-                play_audio(audio_file_path)
-                remove_temp_files(audio_file_path)
-            except Exception as e:
-                st.error(f"Error: Failed to generate or play audio - {e}")
-
-            conversation_history.append({"role": "user", "content": input_text})
-            conversation_history.append({"role": "assistant", "content": response_text})
+        conversation_history.append({"role": "user", "content": input_text})
+        conversation_history.append({"role": "assistant", "content": response_text})
 
 if __name__ == "__main__":
     main(stop_keyword="stop", exit_keyword="exit")
