9aa60087751f633332a15ac508a0a66203b30a0b
Bathiya Ranasinghe <90421548+BathiyaRanasinghe@users.noreply.github.com>
2024-01-07 23:11:49 -0600

    now recognizes the keyword CareBot

--- assistance_detector.py
+++ assistance_detector.py
@@ -1,5 +1,5 @@
 import re
 
 def check_for_assistance(text):
-    pattern = re.compile(r'\b(?:help|assistance|emergency|urgent|pain|hurt|fall(?:en)?|accident|trouble|not feeling well|sick|medicine|doctor|nurse|can\'t get up|need someone|come quickly|help me please|I need help|it\'s an emergency|I\'ve fallen and I can\'t get up|I\'m in pain|I need my medication|I feel sick|I need a doctor|something\'s wrong)\b', re.IGNORECASE)
+    pattern = re.compile(r'\b(?:help|assistance|emergency|urgent|pain|hurt|fall(?:en)?|accident|trouble|not feeling well|sick|medicine|doctor|nurse|can\'t get up|need someone|come quickly|help me please|I need help|it\'s an emergency|I\'ve fallen and I can\'t get up|I\'m in pain|I need my medication|I feel sick|I need a doctor|something\'s wrong|carebot)\b', re.IGNORECASE)
     return bool(pattern.search(text))
