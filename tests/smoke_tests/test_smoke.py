1feb84cf95696233c852570ca1af83e31f6ecaa5
Bathiya Ranasinghe <90421548+BathiyaRanasinghe@users.noreply.github.com>
2024-01-21 14:13:53 -0600

    added testing directory

---
+++ tests/smoke_tests/test_smoke.py
@@ -0,0 +1,14 @@
+import pytest
+import openai
+
+from chatGPT_prompts import generate_response
+
+
+@pytest.mark.smoke
+def test_openai_version():
+    assert openai.__version__ == "0.28.0"
+
+
+def test_gpt_response():
+    assert len(generate_response("Hi ChatGPT", [], 1)) != 0
+_
