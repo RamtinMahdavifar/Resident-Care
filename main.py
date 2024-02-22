c487e0aa1284f4ecfcb8b370c537835515e0f7ee
Bathiya Ranasinghe <90421548+BathiyaRanasinghe@users.noreply.github.com>
2024-02-21 20:55:20 -0700

    refactored out voice recognition functions to a seprate file

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import os
 
 import numpy as np
-import pyaudio
 import pygame
 from TTS.api import TTS
 from vosk import KaldiRecognizer, Model
 
-from keyword_detector import check_for_keywords
 from chatgpt_prompts import generate_response, summarize_conversation_history
 from sms_twilio import send_mms
+from voice_recognition import transcribe_audio, listen_for_keywords
 
 tts = TTS(model_name="tts_models/en/ljspeech/tacotron2-DDC_ph")
 
@@ -28,26 +27,26 @@ def beep(frequency, duration):
     duration (int): The duration of the beep in milliseconds.
     """
     # Constants
-    SAMPLE_RATE = 44100  # Sample rate in Hertz
+    sample_rate = 44100  # Sample rate in Hertz
 
     # Conversion factor from milliseconds to seconds
-    MILLISECONDS_TO_SECONDS = 1000.0
+    milliseconds_to_seconds = 1000.0
 
-    CHANNELS = 2  # Number of audio channels
-    BITS_PER_SAMPLE = 16  # Number of bits per audio sample
-    MAX_SAMPLE_VALUE = 2 ** (
-                BITS_PER_SAMPLE - 1) - 1  # Maximum value for a sample
+    channels = 2  # Number of audio channels
+    bits_per_sample = 16  # Number of bits per audio sample
+    max_sample_value = 2 ** (
+            bits_per_sample - 1) - 1  # Maximum value for a sample
 
     # Generate a sound buffer with the given frequency
-    n_samples = int(round(duration * SAMPLE_RATE / MILLISECONDS_TO_SECONDS))
-    buf = np.zeros((n_samples, CHANNELS), dtype=np.int16)
+    n_samples = int(round(duration * sample_rate / milliseconds_to_seconds))
+    buf = np.zeros((n_samples, channels), dtype=np.int16)
 
     for s in range(n_samples):
-        t = float(s) / SAMPLE_RATE  # time in seconds
+        t = float(s) / sample_rate  # time in seconds
 
         # Generate the sound for both left and right channels
         sample_value = int(
-            round(MAX_SAMPLE_VALUE * np.sin(2 * np.pi * frequency * t)))
+            round(max_sample_value * np.sin(2 * np.pi * frequency * t)))
         buf[s][0] = sample_value  # left channel
         buf[s][1] = sample_value  # right channel
 
@@ -57,40 +56,6 @@ def beep(frequency, duration):
     sound.play(-1)
     pygame.time.delay(duration)
     sound.stop()
-
-
-# Define the robot emoji for display purposes
-AI_Response = "AI Response"
-
-
-def transcribe_audio():
-    """
-    Transcribe audio input from the default microphone using Vosk local speech
-    recognition.
-
-    Returns:
-    str: The transcribed text.
-    """
-    mic = pyaudio.PyAudio()
-    stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000,
-                      input=True, frames_per_buffer=8192)
-
-    stream.start_stream()
-
-    while True:
-        data = stream.read(4096)
-        if recognizer.AcceptWaveform(data):
-            text = recognizer.Result()
-            formatted_text = text[14:-3]
-
-            if len(formatted_text) == 0:
-                continue
-
-            else:
-
-                stream.close()
-                mic.terminate()
-                return formatted_text
 
 
 def remove_temp_files(file_path):
@@ -103,7 +68,7 @@ def remove_temp_files(file_path):
     os.remove(file_path)
 
 
-def play_audio_pygame(file_path):
+def play_audio_file(file_path):
     """
      Plays an audio file
 
@@ -137,44 +102,14 @@ def process_and_play_response(response_text):
 
     try:
         tts.tts_to_file(text=response_text, file_path=output_file_path)
-        play_audio_pygame(output_file_path)
+        play_audio_file(output_file_path)
 
     finally:
         # Delete the file after playing
         remove_temp_files(output_file_path)
 
 
-def recognize_keywords():
-    """
-     Continuously listens until a keyword or set of keywords are spoken in a
-     sentence
-
-     Returns:
-     str: The sentence where a keyword was detected
-
-     """
-    mic = pyaudio.PyAudio()
-    stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000,
-                      input=True, frames_per_buffer=8192)
-
-    stream.start_stream()
-
-    while True:
-        data = stream.read(4096)
-        if recognizer.AcceptWaveform(data):
-            text = recognizer.Result()
-            formatted_text = text[14:-3]
-
-            if len(formatted_text) == 0:
-                continue
-
-            if check_for_keywords(formatted_text):
-                stream.close()
-                mic.terminate()
-                return formatted_text
-
-
-def main(stop_keyword="stop", exit_keyword="exit"):
+def main():
     """
     Main function to run the Azure Speech Integration with ChatGpt Demo.
 
@@ -184,10 +119,12 @@ def main(stop_keyword="stop", exit_keyword="exit"):
     exit_keyword (str): The keyword to exit the conversation.
     """
 
+    ai_response = "AI Response"
+
     while True:
         print("\nSystem Listening")
 
-        input_text = recognize_keywords()
+        input_text = listen_for_keywords()
         print(f"\nYou: \n{input_text}\n")
 
         conversation_history = []
@@ -195,7 +132,7 @@ def main(stop_keyword="stop", exit_keyword="exit"):
         response_text = generate_response(input_text, conversation_history, 1)
 
         # Process the response and play the response audio
-        print(f"{AI_Response} Assistant:\n")
+        print(f"{ai_response} Assistant:\n")
         process_and_play_response(response_text)
 
         running = True
@@ -209,13 +146,13 @@ def main(stop_keyword="stop", exit_keyword="exit"):
             response_text = generate_response(input_text,
                                               conversation_history, 1)
 
-            print(f"{AI_Response} Assistant:\n")
+            print(f"{ai_response} Assistant:\n")
 
             # Process the response and play the response audio
             process_and_play_response(response_text)
 
             response_text = "Do you require assistance?"
-            print(f"{AI_Response} Assistant:\n")
+            print(f"{ai_response} Assistant:\n")
             process_and_play_response(response_text)
 
             beep(800, 200)
@@ -225,7 +162,7 @@ def main(stop_keyword="stop", exit_keyword="exit"):
 
             if "yes" in input_text.lower():
                 response_text = "Sending SMS, Assistance is on the way!"
-                print(f"{AI_Response} Assistant:\n")
+                print(f"{ai_response} Assistant:\n")
                 process_and_play_response(response_text)
 
                 # Summarising conversation and sending SMS to resident
@@ -249,4 +186,4 @@ def main(stop_keyword="stop", exit_keyword="exit"):
 
 
 if __name__ == "__main__":
-    main(stop_keyword="stop", exit_keyword="exit")
+    main()
