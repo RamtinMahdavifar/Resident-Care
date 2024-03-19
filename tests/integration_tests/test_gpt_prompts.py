0205c6e4750304b5aaee473eada6e14ab60fb99d
Bathiya Ranasinghe <90421548+BathiyaRanasinghe@users.noreply.github.com>
2024-03-18 21:52:54 -0600

    added false cases

--- tests/integration_tests/test_gpt_prompts.py
+++ tests/integration_tests/test_gpt_prompts.py
@@ -10,17 +10,34 @@ def test_openai_version():
     assert openai.__version__ == "0.28.0"
 
 
-def test_is_urgent_assistance_needed():
-    assert (is_urgent_assistance_needed("Hi ChatGPT")) is False
-    assert (is_urgent_assistance_needed("I am tired")) is False
-    assert (is_urgent_assistance_needed("I fell down")) is True
-    assert (is_urgent_assistance_needed("I need my medication")) is True
-    assert (is_urgent_assistance_needed("Ahh my chest hurts and I can't breath"
-                                        )) is True
+# def test_is_urgent_assistance_needed():
+#     assert (is_urgent_assistance_needed("Hi ChatGPT")) is False
+#     assert (is_urgent_assistance_needed("I am tired")) is False
+#     assert (is_urgent_assistance_needed("I fell down")) is True
+#     assert (is_urgent_assistance_needed("I need my medication")) is True
+#     assert (is_urgent_assistance_needed("Ahh my chest hurts and I can't breath"
+#                                         )) is True
 
 
 def test_is_intent_to_end_conversation():
+
+    # false cases
     assert (is_intent_to_end_conversation("Hi ChatGPT")) is False
+    assert (is_intent_to_end_conversation("What your opinion on cats")) is \
+           False
+    assert (is_intent_to_end_conversation("I have a question about"
+                                          "the rules in a boxing match")) is \
+           False
+    assert (is_intent_to_end_conversation("Do you have any suggestions for "
+                                          "what to wear to my graduation")) \
+           is False
+    assert (is_intent_to_end_conversation("Care-Bot what is your role")) \
+           is False
+    assert (is_intent_to_end_conversation("I'm more interested in another "
+                                          "topic")) \
+           is False
+
+    # true cases
     assert (is_intent_to_end_conversation("GoodBye")) is True
     assert (is_intent_to_end_conversation("I don't want to talk anymore")) \
            is True
