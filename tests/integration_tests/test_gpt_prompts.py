85d3906543ea99471d683f3f3037005ee717ae2c
Ramtin Mahdavifar <ramtin@Ramtins-MacBook-Pro.local>
2024-03-24 16:32:26 -0600

    fixed typos

--- tests/integration_tests/test_gpt_prompts.py
+++ tests/integration_tests/test_gpt_prompts.py
@@ -15,15 +15,19 @@ def test_is_urgent_assistance_needed():
     assert (is_urgent_assistance_needed("I am going to sleep")) is False
     assert (is_urgent_assistance_needed("I just fell down right now")) is True
     assert (is_urgent_assistance_needed("I need my medication now")) is True
-    assert (is_urgent_assistance_needed("Ahh my chest hurts and I can't breath")) is True
+    assert (is_urgent_assistance_needed("Ahh my chest hurts and "
+                                        "I can't breath")) is True
 
     # Test cases that fail but should not
     # assert (is_urgent_assistance_needed("I can't wait any longer")) is True
     # assert (is_urgent_assistance_needed("Where am I")) is True
     # assert (is_urgent_assistance_needed("I am leaving")) is True
-    # assert (is_urgent_assistance_needed("I can fly out of this window")) is True
-    # assert (is_urgent_assistance_needed("I need to use the washroom now")) is True
-    # assert (is_urgent_assistance_needed("I am going to take my medication early")) is True
+    # assert (is_urgent_assistance_needed("I can fly out of "
+    #                                     "this window")) is True
+    # assert (is_urgent_assistance_needed("I need to use "
+    #                                     "the washroom now")) is True
+    # assert (is_urgent_assistance_needed("I am going to "
+    #                                     "take my medication early")) is True
 
 
 def test_is_intent_to_end_conversation():
@@ -60,8 +64,10 @@ def test_is_intent_to_end_conversation():
            is True
 
     # Test cases that fail but should not
-    # assert (is_intent_to_end_conversation("I don't want to talk to him anymore")) is False
-    # assert (is_intent_to_end_conversation("I don't want to talk to her anymore")) is False
+    # assert (is_intent_to_end_conversation("I don't want to "
+    #                                       "talk to him anymore")) is False
+    # assert (is_intent_to_end_conversation("I don't want to "
+    #                                       "talk to her anymore")) is False
     # assert (is_intent_to_end_conversation("Shush")) is True
     # assert (is_intent_to_end_conversation("Shh")) is True
     # assert (is_intent_to_end_conversation("I have to run")) is True
